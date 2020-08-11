a153a1abc22575d87ef2b6bd86f488f4eb0db310
kessyy <kather946@gmail.com>
2020-08-11 15:51:04 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
 
-const container = document.querySelector(".container");
+const container = document.querySelector(".main-container, .container");
 
 hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
